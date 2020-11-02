7b9b53a0bbf0a6c191189c5780fdcaabcf89b398
Daniel-darnell <darnelldanny1997@gmail.com>
2020-11-02 12:14:31 +0300

    my web application

---
+++ config.py
@@ -0,0 +1,44 @@
+import os
+
+class Config:
+
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Danny@localhost/pitch_test'
+    print (SQLALCHEMY_DATABASE_URI, "00000")
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 2435
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    SUBJECT_PREFIX = 'pitch'
+    SENDER_EMAIL = 'darnelldanny1997@gmail.com'
+    
+# simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+    @staticmethod
+    def init_app(app):
+        pass
+
+class ProdConfig(Config):
+    # DEBUG = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Danny@localhost/pitch_test'
+
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Danny@localhost/pitch_test'
+    # DEVELOPMENT = True
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig,
+'test':TestConfig
+
+}
---
+++ start.sh
@@ -0,0 +1,6 @@
+export MAIL_USERNAME='darnelldanny1997@gmail.com'
+export MAIL_PASSWORD= 'Danny1997'
+export SECRET_KEY='Darnell'
+export DATABASE_URL= "postgresql+psycopg2://moringa:Danny@localhost/pitch_test"
+python3.8 manage.py server  
+
